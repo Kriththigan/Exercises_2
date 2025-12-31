2b3a7909e828a10ff06f7c600b8493085286f60b
Kriththigan <kriththiganklp10@gmail.com>
2025-12-31 19:13:58 +0530

    Commit and Push

---
+++ src/StringDemo.java
@@ -0,0 +1,7 @@
+public class StringDemo {
+    public static void main(String[] args){
+        String sentence =  "Welcome to Java";
+        System.out.println("Length of the string: " + sentence.length());
+        System.out.println("First Character: " + sentence.charAt(0));
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    int x = 10;
+
+    public static void main(String[] args){
+        Test obj = new Test();
+        obj.printValue();
+    }
+    void printValue(){
+        System.out.println(x);
+    }
+}
